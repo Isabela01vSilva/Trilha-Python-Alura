7e65c04a33170fd60d9ab8fa3c18cf1e0ee3c8e5
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-20 17:37:05 -0300

    pasta pratica com 1 exercicio

---
+++ sabor-express/Pratica/ex_input.py
@@ -0,0 +1,14 @@
+print('1. Imprimir uma frase')
+print('Python na Escola de Programação da Alura\n')
+
+print('2. Armazenar nome e idade')
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print(f'Meu nome é {nome} e tenho {idade} anos\n')
+
+print('3. Imprimir uma palavra com as letras separadas')
+print('A','L','U','R','A',sep='\n')
+
+print('\n4. Imprimir o valor de pi')
+pi_arredondado = 3.14159
+print(f"O valor arredondado de pi é: {pi_arredondado:.2f}")
