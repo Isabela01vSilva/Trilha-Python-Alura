882952fdc2c923e060004a4cb08cd511b03ab905
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-21 11:41:15 -0300

    adicionando mais uma pratica

--- sabor-express/Pratica/ex_input.py
+++ sabor-express/Pratica/ex_input.py
@@ -11,4 +11,16 @@ print('A','L','U','R','A',sep='\n')
 
 print('\n4. Imprimir o valor de pi')
 pi_arredondado = 3.14159
-print(f"O valor arredondado de pi é: {pi_arredondado:.2f}")
+print(f"O valor arredondado de pi é: {pi_arredondado:.2f}") 
+
+print('\n 5. Exibir o tipo de genero')
+def classificar_musica(genero_favorito, genero_musica):
+    if genero_favorito == genero_musica:
+        return 'recomendada'
+    elif genero_favorito == 'Pop' or genero_favorito == 'Rock':
+        return 'neutra'
+    else:
+        return 'não recomendada'
+
+resultado = classificar_musica('Pop', 'Rock')
+print(resultado)
