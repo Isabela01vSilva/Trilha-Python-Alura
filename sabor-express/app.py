bb5eb520f762b8d1fef87ce2bee76f0505a8a653
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-21 11:18:47 -0300

    Funções e Import

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,6 @@
-print('Sabor Express\n')
+import os
 
+print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurantes')
 print('3. Ativar restaurante')
@@ -8,6 +9,10 @@ print('4. Sair\n')
 opcao_escolhida = int(input('Escolha uma opção: '))
 #opcao_escolhida = int(opcao_escolhida)
 
+def encerrar_app():
+    os.system('cls') 
+    print('Encerrar o App\n')
+
 if opcao_escolhida == 1: 
     print('Cadastrar restaurante')
 elif opcao_escolhida == 2:
@@ -15,4 +20,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
-    print('Encerrando o programa')
+    encerrar_app()
