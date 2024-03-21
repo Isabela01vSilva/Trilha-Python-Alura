18e9311b958e0886817636f9fd804a273542b142
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-21 15:28:53 -0300

    Refatorando o código

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -12,31 +12,33 @@ def exibir_opcoes_():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls') 
-    print('Encerrar o App\n')
+    exibir_subtitulo('Finalizar App')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
 
 def opcao_invalida():
     print('Opção Inválida!\n')
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantess')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastro com sucesso\n')
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    os.system('cls')
-    print('Listando os restaurantes\n')
+    exibir_subtitulo('Listando os restaurantes')
     for restaurante in restaurantes:
         print(f'.{restaurante}')
-
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 def escolher_opcoes():
     try:
