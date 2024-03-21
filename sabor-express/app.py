dc7ad628b9bfb1e55705aa53d383517cebba56c1
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-21 11:26:57 -0300

    Aprofundando em funções

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,23 +1,35 @@
 import os
 
-print('Sabor Express\n')
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print('Sabor Express\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-#opcao_escolhida = int(opcao_escolhida)
+def exibir_opcoes_():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def encerrar_app():
     os.system('cls') 
     print('Encerrar o App\n')
 
-if opcao_escolhida == 1: 
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    encerrar_app()
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    #opcao_escolhida = int(opcao_escolhida)
+
+    if opcao_escolhida == 1: 
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        encerrar_app()
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes_()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
