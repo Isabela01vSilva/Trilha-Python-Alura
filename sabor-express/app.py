4c43f416a0637b5b300c163b84fcfc92f559d9c3
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-21 15:01:31 -0300

    try except

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -9,24 +9,35 @@ def exibir_opcoes_():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-def encerrar_app():
+def finalizar_app():
     os.system('cls') 
     print('Encerrar o App\n')
 
-def escolher_opcoes():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    #opcao_escolhida = int(opcao_escolhida)
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        encerrar_app()
+def escolher_opcoes():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        #opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
     
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes_()
     escolher_opcoes()
