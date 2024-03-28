d3ec46f152b13a67a4700effd796b31a25923751
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-28 16:55:08 -0300

    Docusign a classe restaurante

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,9 +1,17 @@
 from modelos.avaliacao import Avaliacao
 
 class Restaurante:
+    '''Representa um restaurante e suas características.'''
+    
     restaurantes = []
 
     def __init__(self, nome, categoria):
+        '''Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (str): O nome do restaurante.
+        - categoria (str): A categoria do restaurante.'''
+        
         self._nome = nome.title() #O title deixa as primeiras letrar em Maiúsculas 
         self._categoria = categoria.upper() #Upper deixa todas as letras em Maiúsculas
         self._ativo = False
@@ -11,10 +19,12 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
+        '''Retorna uma representação em string do restaurante.'''
         return f'{self._nome} | {self._categoria}'
     
     @classmethod
     def listar_restaurantes(cls):
+        '''Exibe uma lista formatada de todos os restaurantes.'''
         nome_restaurante = 'Nome do restaurante'
         categoria_restaurante = 'Categoria'
         media_restaurante = 'Avaliação'
@@ -26,18 +36,27 @@ class Restaurante:
 
     @property
     def ativo(self):
+        '''Retorna um símbolo indicando o estado de atividade do restaurante.'''
         return '⌧' if self._ativo else '☐'
 
     def alternar_estado(self):
+        '''Alterna o estado de atividade do restaurante.'''
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
+        '''Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (str): O nome do cliente que fez a avaliação.
+        - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
+        '''
         if 0 < nota <= 5 :
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
 
     @property
     def media_avaliacao(self):
+        '''Calcula e retorna a média das avaliações do restaurante.'''
         if not self._avaliacao:
             return 'Não tem avaliações'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
