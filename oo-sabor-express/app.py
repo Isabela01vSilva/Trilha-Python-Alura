d3eb9b0005f7b71114a304874764bc2378b0fb5d
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-28 16:39:20 -0300

    Aprimorando projeto final

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -4,6 +4,7 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Guinga', 10)
 restaurante_praca.receber_avaliacao('Duda', 8)
 restaurante_praca.receber_avaliacao('Anna', 4)
+restaurante_sushi = Restaurante('Sushi', 'Japones')
 
 def main():
     Restaurante.listar_restaurantes()
