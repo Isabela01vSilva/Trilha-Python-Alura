4dcba1e2266d4657d4ed1c01bd6fb5e541c7a0e4
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-28 15:33:13 -0300

    Property

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -4,21 +4,26 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        nome_restaurante = 'Nome do restaurante'
+        categoria_restaurante = 'Categoria'
+        print(f'{nome_restaurante.ljust(25)} | {categoria_restaurante.ljust(25)} | Status')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
-    
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+
 restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_pizza = Restaurante('Pizza Place','Italiano')
 
 Restaurante.listar_restaurantes()
 #O 'dir()' mostrará tudo: os atributos, métodos e propriedades de um objeto
 #print(dir(restaurante_praca))
-
-#Exibi o que foi definido 
