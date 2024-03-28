acabbc07b38e31a0c66239ac5bf027129e08ced3
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-28 15:43:40 -0300

    Aprofundando em propriedades

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -2,27 +2,27 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title() #O title deixa as primeiras letrar em Maiúsculas 
+        self._categoria = categoria.upper() #Upper deixa todas as letras em Maiúsculas
         self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         nome_restaurante = 'Nome do restaurante'
         categoria_restaurante = 'Categoria'
         print(f'{nome_restaurante.ljust(25)} | {categoria_restaurante.ljust(25)} | Status')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_pizza = Restaurante('Pizza Place','Italiano')
+restaurante_pizza = Restaurante('Pizza place','Italiano')
 
 Restaurante.listar_restaurantes()
 #O 'dir()' mostrará tudo: os atributos, métodos e propriedades de um objeto
