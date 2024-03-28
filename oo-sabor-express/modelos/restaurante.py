4959588430cf5b4b93087c21093b08bf284d8492
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-28 15:48:11 -0300

    Classes no def listar_restaurantes

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -10,20 +10,23 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         nome_restaurante = 'Nome do restaurante'
         categoria_restaurante = 'Categoria'
         print(f'{nome_restaurante.ljust(25)} | {categoria_restaurante.ljust(25)} | Status')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
 
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
 restaurante_praca = Restaurante('Praça','Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza place','Italiano')
 
 Restaurante.listar_restaurantes()
-#O 'dir()' mostrará tudo: os atributos, métodos e propriedades de um objeto
-#print(dir(restaurante_praca))
