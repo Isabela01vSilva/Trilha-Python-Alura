94d5556bf8a451ec75341712d818c4855cf01626
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-04-02 12:29:13 -0300

    Adicionando metodo de desconto (Polimorfismo)

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Laranja', 5.00, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_pao = Prato('Pão', 3.00, 'Pão Doce com creme')
+prato_pao.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_pao)
 
